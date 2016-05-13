1c3368855f71ed520f8570524ee7d1d086306e02
Franck Verrot <franck@verrot.fr>
2016-05-13 11:49:21 -0700

    Stop assigning ivar before creating objects
    This is a left-over from a long time ago...

--- lib/blake2/key.rb
+++ lib/blake2/key.rb
@@ -9,15 +9,15 @@ class Blake2
     end
 
     def self.from_string(str)
-      new(@bytes = str.bytes)
+      new(str.bytes)
     end
 
     def self.from_hex(hex_str)
-      new(@bytes = [hex_str].pack("H*").bytes)
+      new([hex_str].pack("H*").bytes)
     end
 
     def self.none
-      new(@bytes = [])
+      new([])
     end
   end
 end
