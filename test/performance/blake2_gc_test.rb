44545e6df3c8172ce701cd7fb6eceee3dce2b078
Franck Verrot <franck@verrot.fr>
2016-05-14 10:51:48 -0700

    Ensure test is named properly named
    And that it does what it claims to do

--- test/performance/blake2_gc_test.rb
+++ test/performance/blake2_gc_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class Blake2VectorTest < MiniTest::Test
+class Blake2GCTest < MiniTest::Test
   def test_a_million_iteration
-    100000.times do |i|
+    1_000_000.times do |i|
       Blake2.new(32, Blake2::Key.none).digest('abc', :to_hex)
     end
   end
